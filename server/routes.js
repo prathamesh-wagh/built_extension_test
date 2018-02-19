eb0ad3c6f95cf0766acf8b333bd32fed488f7335
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-19 16:08:06 +0530

    1. Changes routes.

--- server/routes.js
+++ server/routes.js
@@ -85,7 +85,7 @@ module.exports = {
 					
 					req.logger.log(reqPayload)
 
-					req.logger.log(req.bobjekt)
+					req.logger.log(req.bobjekt.get("status"))
 
 					if(req.bobjekt.get("status") == "closed") {
 						return that.resError(req, res, {
