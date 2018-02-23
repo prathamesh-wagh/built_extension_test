e5b36e6ad869a3cbe06cab22d9d63a6829280f5f
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-23 19:09:52 +0530

    1. Changes app.js, routes.js, index.html and package.json.

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var app = Built.App('blt8cb659c05289f7a3')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
-// blt_ext_default_test - extension key
+// blt_ext_default_test - extension key - blt_ext_third_party_secret, blt_ext_third_party
 // web hook url - https://test-stag-api.built.io/v1/extensions/blt_ext_default/redeploy?application_api_key=bltc4f7ed1f03053fe8
 
 var extensionSDK = app.Extension({
 	extension_key  : 'blt_ext_default_test_app',
 	secret_key     : 'app_test_123',
+	// extension_key  : 'blt_ext_third_party',
+	// secret_key     : 'blt_ext_third_party_secret',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
 })
