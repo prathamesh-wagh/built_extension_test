ab9234477f63f382d64ee70341b2a10edf3d0bdd
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-23 11:13:03 +0530

    1. Changes app.js and index.html.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var Built = require('built-extension-sdk')
 var app = Built.App('blt8cb659c05289f7a3')
 .setHost("dev-api.built.io")
 .setProtocol("https")
-.setMasterKey('bltf4a27ff1b7ce3a59')
+.setMasterKey('bltf4a27ff1b7ce3a5666')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
@@ -12,7 +12,7 @@ var app = Built.App('blt8cb659c05289f7a3')
 // web hook url - https://test-stag-api.built.io/v1/extensions/blt_ext_default/redeploy?application_api_key=bltc4f7ed1f03053fe8
 
 var extensionSDK = app.Extension({
-	extension_key  : 'blt_ext_default_test_app1',
+	extension_key  : 'blt_ext_default_test_app',
 	secret_key     : 'app_test_123',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
