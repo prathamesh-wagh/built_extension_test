9fe70f3a6a3543df464985d106075e0f75c3d051
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-23 11:55:01 +0530

    1. Changes app.js and index.html.

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ var app = Built.App('blt8cb659c05289f7a3')
 // blt_ext_default_test - extension key
 // web hook url - https://test-stag-api.built.io/v1/extensions/blt_ext_default/redeploy?application_api_key=bltc4f7ed1f03053fe8
 
-var extensionSDK = app.Extension()
+var extensionSDK = app.Extension({
+	extension_key  : 'blt_ext_default_test_app',
+	secret_key     : 'app_test_123',
+	static         : __dirname + '/client',
+	routes         : require('./server/routes')
+})
 
-return extensionSDK.start(8080)
+return extensionSDK.start(9000)
 
 // Wrong code
 // var Built = require('built-extension-sdk')
