561cbfff1f1d5e2ef440e5ba998e48463b591208
prathamesh-wagh <prathamesh.wagh@raweng.com>
2018-02-23 11:17:48 +0530

    1. Changes app.js and index.html.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var Built = require('built-extension-sdk')
 
 // Initiate application
 var app = Built.App('blt8cb659c05289f7a3')
-.setHost("dev-api.built.io")
+.setHost("devdsdsdfsdf-api.built.io")
 .setProtocol("https")
-.setMasterKey('bltf4a27ff1b7ce3a5666')
+.setMasterKey('bltf4a27ff1b7ce3a59')
 
 // 574df6c8fb2034b1603d792e8e2922268779c456 - access token
 // bltc88f3d42ad8a728c - secret key
